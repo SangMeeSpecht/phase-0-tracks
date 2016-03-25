9b0edd1dfed18832a3a74452d6bb7015a62b79e1
SangMeeSpecht <spechtse@miamioh.edu>
2016-03-25 11:57:16 -0500

    update pseudocode and add else messageoption

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,33 +1,32 @@
 =begin
-
 An encrypt method that advances every letter of a string one letter forward.
+-define method with parameter, which will contain the password encrypted
+  -create counter that will keep track of how many times the string has gone through the loop
+  -create a loop that will loop through each letter of the string argument
+  -start with string character with smallest index number 
+  -change letter to one letter forward
+  -store letter in a varible
+  -increase loop counter by one
+  -keep looping until all the characters have been assigned a new letter
+  -print encrypted password
+  -if string contains "ab", substitute with "a" and return string, else return string
 
-create a variable with string that you're trying to encrypt.
 
-define method in order to loop through string.
--utilize .length method so we know how many times to loop through.
--print each letter
--use .next method
+A decrypt method that reverses the process above. 
+  -create counter that will keep track of how many times the string has gone through the loop
+  -create a loop that will loop through each letter of the string argument
+  -if string contains "!", substitute with " " and return string, else return string 
+  -start with string character with smallest index number 
+  -change letter to one letter backward, using the alphabet in a variable 
+  -store letter in a varible
+  -increase loop counter by one
+  -keep looping until all the characters have been assigned a new letter
+  -print decrypted password
 
-
-So "abc" would become "bcd". For now, you can assume lowercase input and output. 
-
-A decrypt method that reverses the process above. Note that Ruby doesn't have a built-in method for going backward one letter. How can you find out where a letter is in the alphabet, then access the letter right before it? Hint: In the IRB session above, you learned how to ask a string for the index of a letter. "abcdefghijklmnopqrstuvwxyz" counts as a string.
-
-define method in order to loop through string
- -utilizie .length method so we know how many times to loop through
- -have some kind of counter to count how many times loop has completed
- -loop takes one letter at a time (in order) and changes it to the letter right before it (example: "b" would become "a")
- -puts letter on screen
- -does loop again as many times as needed
-
--variable with a string value of all alphabet letters in order
 =end 
 
 puts "Would you like to decrypt or encrypt a password?"
 msg_type = gets.chomp
-
-#START DECRYPTING
 
 if msg_type == "decrypt"
   def decrypt(password)
@@ -50,14 +49,11 @@ if msg_type == "decrypt"
     end
     index +=1
   end
-end
-  
+  end
   puts "What is your password you would like to decrypt?"
   pw = gets.chomp
   decrypt(pw)
-
-#STAR ENCRYPTING
-
+  
 elsif msg_type == "encrypt"
   def encrypt(password)
   index= 0
@@ -70,9 +66,15 @@ elsif msg_type == "encrypt"
     else
       return password
     end
-end
-
+  end
   puts "What is your password you would like to encrypt?"
   pw = gets.chomp
-  encrypt(pw)
+  encrypt(pw)    
+
+else
+	puts "Are you a spy?!  Program will self destruct in..."
+	puts "3..."
+	puts "2..."
+	puts "1..."
+	puts "BOOM!"
 end
