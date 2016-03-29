25e25f61e03e095962955276b88ab93c3bf23ce1
SangMeeSpecht <spechtse@miamioh.edu>
2016-03-28 19:55:35 -0500

    Add Release 2

--- ruby/arrays_drill.rb
+++ ruby/arrays_drill.rb
@@ -1,4 +1,15 @@
-food = ["mango", "cheese", "pork belly", "ramen", "egg"]
+def return_array (param1, param2, param3)
+  rtrn_array = [param1, param2, param3]
+end
+
+
+# This method will take in an array and an item to add into passed array
+def add_item(array_param, new_item)
+  array_param << new_item
+end
+
+food = []
+food += ["mango", "cheese", "pork belly", "ramen", "egg"]
 print food
 
 food.delete_at(2)
@@ -16,3 +27,6 @@ puts "Is \"ramen\" in the array? #{in_array}"
 drinks = ["water", "beer", "wine"]
 dinner = food + drinks
 print dinner
+
+p return_array(1, "hello", 4)
+p add_item([1,2,3], "new item")
