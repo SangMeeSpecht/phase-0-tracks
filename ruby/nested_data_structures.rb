6547b837a4fcb0c416da7bc23a9b1a0e0fdf4fb6
SangMeeSpecht <spechtse@miamioh.edu>
2016-03-29 15:37:07 -0500

    Add nested data elements

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,40 +1,77 @@
-Chicago = {
-	Streeterville: {
-		walk_score: , 
+chicago_hoods = {
+	streeterville: {
+		walk_score: 98, 
+		rent: "$$$/$$$",
 		restaurants: [
-		
+			"Signature Lounge",
+			"DiSotto Enoteca",
+			"The Purple Pig"
 		], 
 		attractions: [
+			"John Hancock Center",
+			"Water Tower Place",
+			"Magnificent Mile"
 		]
 	},
-	South_Loop: {
-		walk_score: , 
+	south_loop: {
+		walk_score: 91, 
+		rent: "$$/$$$",
 		restaurants: [
-
+			"Eleven City Diner",
+			"Umai",
+			"Nepal House",
+			"Bongo Room"
 		], 
 		attractions: [
+			"Buckingham Fountain",
+			"The Field Museum",
+			"Shedd Aquarium",
+			"Adler Planetarium"
 		]
 	},
-	Lakeview: {
-		walk_score: , 
+	lakeview: {
+		walk_score: 90, 
+		rent: "$$/$$$",
 		restaurants: [
+			"Crisp",
+			"BIG & little's Restaurant",
+			"Rice'N Bread"
 		], 
 		attractions: [
+			"Belmont Harbor",
+			"Blue Man Group",
+			"The Vic Theatre"
 		]
 	},
-	Chinatown: {
-		walk_score: , 
+	chinatown: {
+		walk_score: 81, 
+		rent: "$/$$$",
 		restaurants: [
+			"Triple Crown Restuarant",
+			"Joy Yee Noodle"
 		], 
 		attractions: [
+			"Pui Tak Center",
+			"Chinese American Museum of Chicago"
 		]
 	},
-	Hyde_Park: {
-		walk_score: , 
+	hyde_park: {
+		walk_score: 84, 
+		rent: "$/$$$",
 		restaurants: [
+			"Medici on 57th",
+			"Salonica",
+			"Noodles Etc"
 		], 
 		attractions: [
+			"The University of Chicago",
+			"Museum of Science and Industry",
+			"Oriental Institute Museum"
 		]
 	},
 }
 
+puts chicago_hoods[:streeterville]
+puts chicago_hoods[:hyde_park][:walk_score]
+puts chicago_hoods[:lakeview][:restaurants]
+puts chicago_hoods[:chinatown][:restaurants][0]
