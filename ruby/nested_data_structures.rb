4873138de954817a7ce63bb18c5b1d77d1f970af
SangMeeSpecht <spechtse@miamioh.edu>
2016-03-29 14:05:20 -0500

    Add skeleton of nested data structures

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,40 @@
+Chicago = {
+	Streeterville: {
+		walk_score: , 
+		restaurants: [
+		
+		], 
+		attractions: [
+		]
+	},
+	South_Loop: {
+		walk_score: , 
+		restaurants: [
+
+		], 
+		attractions: [
+		]
+	},
+	Lakeview: {
+		walk_score: , 
+		restaurants: [
+		], 
+		attractions: [
+		]
+	},
+	Chinatown: {
+		walk_score: , 
+		restaurants: [
+		], 
+		attractions: [
+		]
+	},
+	Hyde_Park: {
+		walk_score: , 
+		restaurants: [
+		], 
+		attractions: [
+		]
+	},
+}
+
