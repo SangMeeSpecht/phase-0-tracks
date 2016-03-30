c9bfb18be75f0cebd1a2501414784e35ae572bd8
SangMeeSpecht <spechtse@miamioh.edu>
2016-03-29 19:32:08 -0500

    Add new iterator that includes exceptions

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,17 +1,29 @@
 puts "Please enter your full name."
 name = gets.chomp
-
-
 split_name = name.split("")
+
 split_name.map! { |letter|
-	case letter
-	  when "a" then letter = "e"
-	  when "e" then letter = "i"
-	  when "i" then letter = "o"
-	  when "o" then letter = "u"
-	  when "u" then letter = "a"
-	  when " " then letter = " "
-	  else letter.next
+	if letter == "a" || letter == "e" || letter == "i" || letter == "o" || letter == "u"
+		vowel = "uoiea"		
+    	current_index = vowel.index(letter)
+  		vowel[current_index - 1]
+  	elsif letter == "A" || letter == "E" || letter == "I" || letter == "O" || letter == "U"
+		vowel = "UOIEA"		
+    	current_index = vowel.index(letter)
+  		vowel[current_index - 1]
+  	elsif letter == "d" || letter == "h" || letter == "n" || letter == "t"
+  		letter.next.next
+ 	elsif letter == "D" || letter == "H" || letter == "N" || letter == "T"
+  		letter.next.next
+  	elsif letter == "z"
+  		letter = "b"
+  	elsif letter == "Z"
+  		letter = "B"
+  	elsif letter == " "
+  		letter = " "
+  	else
+  		letter.next
 	end
 }
 
+letter_switch = split_name.join("")
