4c848e98430aaa625924291fe3e759903c382493
SangMeeSpecht <spechtse@miamioh.edu>
2016-03-30 15:05:48 -0500

    Add Release 2

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -2,10 +2,14 @@
 puts "Welcome to the TOP-SECRET Alias Management System"
 puts "Please enter the full name you would like to change."
 puts "Enter 'quit' when finished."
+
+# Create empty hash to store key (real name) value (secret identity) pairs
+id_list = {}
+
 finished = false
 
 until finished == true
-  old_name = gets.chomp.downcase
+  old_name = gets.chomp
 
   if old_name == "quit"
     puts "Thank you for using the Alias Management System.  Goodbye!"
@@ -13,7 +17,8 @@ until finished == true
   else 
     def alias_manager(name)      
 # Split original string into two strings
-      swap_name = name.split(" ")
+      down_name = name.downcase
+      swap_name = down_name.split(" ")
 # Swap the first string with the second
       swap_name.insert(0, swap_name.delete_at(1))
 
@@ -24,10 +29,18 @@ until finished == true
       string.capitalize!
       }
 # Join two strings into one and print
-      puts swap_name.join(" ")
+      new_name = swap_name.join(" ")
+      p new_name
+      new_name 
     end
 
-    alias_manager(old_name)
+    new_name = alias_manager(old_name)
+    
+# Store names in empty hash
+    id_list[old_name] = new_name
+
     puts "Please enter another name to be changed.  Enter 'quit' if finished."
   end
 end
+
+id_list.each { |old_name, new_name| puts "The secret identity of '#{old_name}' is '#{new_name}'" }
