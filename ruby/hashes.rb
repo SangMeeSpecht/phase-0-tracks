feca35bd0d637e638a88cc0b601de39c9cb7cf5d
SangMeeSpecht <spechtse@miamioh.edu>
2016-04-03 17:15:52 -0500

    Fix pseudocode organization

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,54 +1,59 @@
-#Create empty hash to store key and value pairs
+=begin
+Create empty hash to store key and value pairs
+
+Ask for client name
+Store answer in variable
+Add variable as key and variable value as key value into hash
+
+Ask for client age
+Store answer in variable (change to integer)
+Add variable as key and variable value as key value into hash
+
+Ask for client children number
+Store answer in variable (change to integer)
+Add variable as key and variable value as key value into hash
+
+Ask for client decor theme preference
+Store answer in variable 
+Add variable as key and variable value as key value into hash
+
+Ask if client has pets
+Store answer in variable 
+Add variable as key and variable value as key value into hash
+
+Print hash 
+
+Ask designer if they would like to update a key 
+Store answer in variable
+If "yes", update value, if "no", then skip to next step, if other, then put "error" and loop to previous question
+
+Print updated hash 
+=end
+
 client_data = {}
 
-
-#Ask for client name
-#Store answer in variable
-#Add variable as key and variable value as key value into hash
 puts "Please enter your client's full name."
 name = gets.chomp
 client_data[:name] = name
 
-
-#Ask for client age
-#Store answer in variable (change to integer)
-#Add variable as key and variable value as key value into hash
 puts "How old is your client?"
 age = gets.chomp.to_i
 client_data[:age] = age
 
-
-#Ask for client children number
-#Store answer in variable (change to integer)
-#Add variable as key and variable value as key value into hash
 puts "How many children does your client have?"
 children = gets.chomp.to_i
 client_data[:number_of_children] = children 
 
-
-#Ask for client decor theme preference
-#Store answer in variable 
-#Add variable as key and variable value as key value into hash
 puts "What is your client's decor theme preference.  Please enter only one."
 decor_theme = gets.chomp
 client_data[:decor_theme] = decor_theme
 
-
-#Ask if client has pets
-#Store answer in variable 
-#Add variable as key and variable value as key value into hash
 puts "Does your client have any pets?  Enter 'true' or 'false'." 
 pets = gets.chomp
 client_data[:pets] = pets 
 
-
-#Print hash 
 puts client_data
 
-
-#Ask designer if they would like to update a key 
-#Store answer in variable
-#If "yes", update value, if "no", then skip to next step, if other, then put "error" and loop to previous question
 puts "Would you like to make any information updates? (Y/N)"
 update = ""
 
@@ -69,6 +74,4 @@ until update == "Y" || update == "N"
 	end
 end
 
-
-#Print updated hash 
 puts client_data
