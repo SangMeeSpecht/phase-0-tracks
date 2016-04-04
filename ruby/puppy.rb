5be7af4000e2ad57e3d830c4d783582ddc80c9e4
SangMeeSpecht <spechtse@miamioh.edu>
2016-04-04 12:31:34 -0500

    Add Release 0

---
+++ ruby/puppy.rb
@@ -0,0 +1,20 @@
+=begin 
+
+SPECIES----------------
+Canis goldenis rextriveus
+
+CHARACTERISTICS--------
+forefeet length: short 
+teeth size: long and pointy 
+head size: huge
+fur color: golden
+bark: grrrr....rawrrrr
+
+BEHAVIOR---------------
+Predator-Mode
+Meal-Smash
+Slopper
+Tail-Whip
+Short-Reach
+
+=end
