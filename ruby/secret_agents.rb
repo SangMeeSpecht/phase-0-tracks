f6c618250154d8ed288803f15cb5723986d69aea
SangMeeSpecht <spechtse@miamioh.edu>
2016-04-07 15:28:09 -0500

    Add initial pseudocode

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,77 +1,53 @@
-=begin
-An encrypt method that advances every letter of a string one letter forward.
--define method with parameter, which will contain the password encrypted
-  -create counter that will keep track of how many times the string has gone through the loop
-  -create a loop that will loop through each letter of the string argument
-  -start with string character with smallest index number 
-  -change letter to one letter forward
-  -store letter in a varible
-  -increase loop counter by one
-  -keep looping until all the characters have been assigned a new letter
-  -print encrypted password
-  -if string contains "ab", substitute with "a" and return string, else return string
+=begin 
+
+next letter method that takes a letter argument
+	IF letter is 'z'
+		RETURN 'a'
+	ELSE
+		call next method 
+END of method
+
+previous letter method that takes a letter argument
+	alphabet string 
+	IF letter is 'a'
+		RETURN 'b'
+	ELSE
+		find index number of letter on alphabet string
+		subtract that index number -1 
+		RETURN letter 
+	END of conditional
+END of method
 
 
-A decrypt method that reverses the process above. 
-  -create counter that will keep track of how many times the string has gone through the loop
-  -create a loop that will loop through each letter of the string argument
-  -if string contains "!", substitute with " " and return string, else return string 
-  -start with string character with smallest index number 
-  -change letter to one letter backward, using the alphabet in a variable 
-  -store letter in a varible
-  -increase loop counter by one
-  -keep looping until all the characters have been assigned a new letter
-  -print decrypted password
+encrypt method that advances every letter of a string forward one letter
+	counter to count number of times looped through each index number
+	UNTIL counter is equal to password length
+		IF letter is a space
+			letter is equal to a space
+		ELSIF letter is a capital letter
+			downcase letter
+			go to next letter 
+			capitalize letter
+		ELSE 
+			go to next letter
+		END of conditional
+		add 1 to counter
+END of method
 
-=end 
+decrypt method that takes ever letter of a string one letter backward
+	counter to count number of times looped through each index number
+	UNTIL counter is equal to password length
+		IF letter is a space
+			letter is equal to a space
+		ELSIF letter is a capital letter
+			downcase letter
+			go to previous letter
+			capitalize lettter
+		ELSE
+			go to previous letter
+		END of conditional
+		add 1 to counter
+	END of loop
+END of method
 
-puts "Would you like to decrypt or encrypt a password?"
-msg_type = gets.chomp
-
-if msg_type == "decrypt"
-  def decrypt(password)
-    index= 0
-    alpha = "abcdefghijklmnopqrstuvwxyz !"
-    password=password.downcase
-    while index < password.length
-      if password[index] == "a"
-    	  password[index] = "z"
-      elsif password[index] == " "
-        password[index] = "!"
-      else
-       alpha_index_number = alpha.index(password[index])
-       index_minus_1 = alpha_index_number - 1
-       password[index] = alpha[index_minus_1]
-      end
-      index +=1
-    end
-    return password
-  end
-  puts "What is your password you would like to decrypt?"
-  pw = gets.chomp
-  decrypt(pw)
-  
-elsif msg_type == "encrypt"
-  def encrypt(password)
-    index= 0
-    while index < password.length
-      password[index] = password[index].next
-      index +=1
-    end
-    if password.include? "ab"
-      return password.gsub!("ab", "a")
-    else
-      return password
-    end
-  end
-  puts "What is your password you would like to encrypt?"
-  pw = gets.chomp
-  encrypt(pw)    
-
-else
-	puts "Are you a spy?!  Program will self destruct in..."
-	puts "3..."
-	puts "2..."
-	puts "1..."
-	puts "BOOM!"
-end
+=end
