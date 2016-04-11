a67717bafbb153d5c5abd2918f83eb8a999a8b9d
SangMeeSpecht <spechtse@miamioh.edu>
2016-04-11 15:44:02 -0500

    Fix string not reversing bug

--- js/explore.js
+++ js/explore.js
@@ -1,23 +1,5 @@
 /*
-
-* Ruby has lots of handy functions like .reverse, but in JavaScript, we mostly have to write our own. In 
-comments, pseudocode a function that takes a string as a parameter and reverses the string. reverse("hello"), 
-for example, should return "olleh". This isn't a task specific to JavaScript -- your pseudocode should be in 
-plain English and use phrases like "for each" instead of JavaScript-specific terms. Remember that you can add 
-strings in JavaScript, so '' + 'a' would result in the string 'a'. Take your time. This is the most valuable 
-skill in programming: being able to think through a problem logically.
-
-* Under your commented pseudocode, implement your function in JavaScript. Test it with driver code if you 
-like. Anytime you'd like to run your code, you can run node explore.js from the js folder in your terminal, 
-similar to how you run Ruby programs.
-
-* Add driver code that calls the function in order to reverse a string of your choice (as long as it's not a 
-palindrome!), and stores the result of the function in a variable.
-
-* Add driver code that prints the variable if some condition is true. The condition can be a silly one, like 
-1 == 1.
-
-declare a function that takes a string as an argument
+declare a function that takes a string as a parameter
 	loop through each character in the string
 		* store empty string in variable 
 		* beginning with the last character in the string, store each character in empty string
@@ -34,13 +16,19 @@ IF some condition is true
 END of conditional
 */
 
+// Reverses string argument
 var reverse = function(string) {
-	for(index = string.length - 1; index >= 0; index --) {
-		var reverseString = "";
-		reverseString += string.substring(index);
+	var reverseString = "";
+	for (index = string.length - 1; index >= 0; index --) {
+		reverseString += string[index];
 	}
-		return reverseString;
-};
+	return reverseString;
+}
 
+// DRIVER CODE
 var reversed = reverse("hello");
 console.log(reversed);
+
+if (1 + 1 == 2) {
+	console.log(reversed);
+}
