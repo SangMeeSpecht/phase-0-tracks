0c55ca82c183617770a39794e52373c523d66b0c
SangMeeSpecht <spechtse@miamioh.edu>
2016-04-13 14:33:22 -0500

    Add Release 1: pseudocode

---
+++ js/algos.js
@@ -0,0 +1,19 @@
+/*
+RELEASE 1
+define array with list of phrases
+
+define function to return longest word in array
+	* define empty hash to push key(word/phrase)/value(length)
+	* loop through each element in the array
+		* calculate the length of the element/string
+		* push the key/value pair to the empty array 
+	* END of loop
+	* loop through filled hash
+		* sort the hash based on the numberical values
+	* END of loop
+	* loop through the sorted hash
+		* RETURN the keys with the highest value(s)
+	* END of loop
+END of function
+*/
+
