cc91fbbb313e564e210177898ae34db0043dc8de
SangMeeSpecht <spechtse@miamioh.edu>
2016-04-13 23:42:08 -0500

    Release 0: refactor pseudocode

--- js/algos.js
+++ js/algos.js
@@ -1,19 +1,15 @@
 /*
-RELEASE 1
-define array with list of phrases
 
-define function to return longest word in array
-	* define empty hash to push key(word/phrase)/value(length)
-	* loop through each element in the array
-		* calculate the length of the element/string
-		* push the key/value pair to the empty array 
+declare array, which contains strings of various lengths
+	
+declare method that returns the longest string in the array
+	* loop through each element in the array while the length of the array is greater than 1
+		* IF the length of the first element is greater than the 2nd
+			* delete the 2nd element
+		* ELSE
+			* delete the 1st element
 	* END of loop
-	* loop through filled hash
-		* sort the hash based on the numberical values
-	* END of loop
-	* loop through the sorted hash
-		* RETURN the keys with the highest value(s)
-	* END of loop
-END of function
+	* RETURN the 1st element of the array
+END of method
+
 */
-
