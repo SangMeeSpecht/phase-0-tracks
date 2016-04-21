f2b0c1026fe7dbfdbb93bd03529926bf53abcc7f
SangMeeSpecht <spechtse@miamioh.edu>
2016-04-21 16:16:49 -0500

    Add pseudocode for renters table

--- databases/85SQLruby.rb
+++ databases/85SQLruby.rb
@@ -9,9 +9,10 @@ require 'sqlite3' and 'faker' gems
 create new SQLite3 database and save in variable
 save data items in hash
 
+LANDLORD TABLE
 create 'landlord' table (if is doesn't already exist) and save in variable
   * see schema for content
-  * END of table
+END of table
 
 execute 'landlord' table 
 
@@ -28,6 +29,11 @@ END of method
 	* add fake 'landlord' data to 'landlord' table using 'faker' gem
 END of loop
 
+RENTERS TABLE
+create 'renters' table (if is doesn't already exist) and save in variable
+  * see schema for content
+END of table
+
 =end
 
 require "sqlite3"
@@ -36,41 +42,41 @@ require "faker"
 db = SQLite3::Database.new("apartment_hunter.db")
 db.results_as_hash = true
 
-create_landlord_table = <<-SQL
-	CREATE TABLE IF NOT EXISTS landlords(
-    id INTEGER PRIMARY KEY,
-    name VARCHAR(255),
-    neighborhood VARCHAR(255),
-    rent INT,
-    bedrooms INT,
-    bathrooms INT,
-    parking BOOLEAN
-	)
-SQL
-
-db.execute(create_landlord_table)
-
+# create_landlord_table = <<-SQL
+	# CREATE TABLE IF NOT EXISTS landlords(
+    # id INTEGER PRIMARY KEY,
+    # name VARCHAR(255),
+    # neighborhood VARCHAR(255),
+    # rent INT,
+    # bedrooms INT,
+    # bathrooms INT,
+    # parking BOOLEAN
+	# )
+# SQL
+# 
+# db.execute(create_landlord_table)
+# 
 # test
-db.execute("INSERT INTO landlords (name, neighborhood, rent, bedrooms, bathrooms, parking) VALUES ('Louise Hudson', 'Lakeview', 1200, 2, 1, 'true')")
-
-def hood_selector
-	chicago = ["Albany Park", "Andersonville", "Avondale", "Beverly", "Boystown", "Bridgeport", "Bronzeville", 
-						 "Chinatown", "Edgewater", "Gold Coast", "Humboldt Park", "Hyde Park", "Irving Park", "Jefferson Park", 
-						 "Kenwood", "Lakeview", "Lincoln Park", "Lincoln Square", "Litte Italy & University Village", 
-						 "Little Village", "Logan Square", "Loop", "Magnificent Mile", "North Center", "North Park", "Old Town", 
-						 "Pilsen", "Portage Park", "Pullman", "River North", "Rogers Park", "Roscoe Village", "South Loop", 
-						 "South Shore", "Streeterville", "Uptown", "West Loop", "West Ridge", "West Town", "Wicker Park/Bucktown", 
-						 "Wrigleyville"]
-  chicago.sample
-end
-
-def create_landlords(db, name, neighborhood, rent, bedrooms, bathrooms, parking)
-	db.execute("INSERT INTO landlords (name, neighborhood, rent, bedrooms, bathrooms, parking) VALUES (?, '#{neighborhood}', '#{rent}', '#{bedrooms}', '#{bathrooms}', '#{parking}')", [name])
-end
-
-99.times do
-	create_landlords(db, Faker::Name.name, hood_selector, rand(500..5000), rand(0..5), rand(0..5), ["true", "false"].sample)
-end
+# db.execute("INSERT INTO landlords (name, neighborhood, rent, bedrooms, bathrooms, parking) VALUES ('Louise Hudson', 'Lakeview', 1200, 2, 1, 'true')")
+# 
+# def hood_selector
+	# chicago = ["Albany Park", "Andersonville", "Avondale", "Beverly", "Boystown", "Bridgeport", "Bronzeville", 
+						 # "Chinatown", "Edgewater", "Gold Coast", "Humboldt Park", "Hyde Park", "Irving Park", "Jefferson Park", 
+						 # "Kenwood", "Lakeview", "Lincoln Park", "Lincoln Square", "Litte Italy & University Village", 
+						 # "Little Village", "Logan Square", "Loop", "Magnificent Mile", "North Center", "North Park", "Old Town", 
+						 # "Pilsen", "Portage Park", "Pullman", "River North", "Rogers Park", "Roscoe Village", "South Loop", 
+						 # "South Shore", "Streeterville", "Uptown", "West Loop", "West Ridge", "West Town", "Wicker Park/Bucktown", 
+						 # "Wrigleyville"]
+  # chicago.sample
+# end
+# 
+# def create_landlords(db, name, neighborhood, rent, bedrooms, bathrooms, parking)
+	# db.execute("INSERT INTO landlords (name, neighborhood, rent, bedrooms, bathrooms, parking) VALUES (?, '#{neighborhood}', '#{rent}', '#{bedrooms}', '#{bathrooms}', '#{parking}')", [name])
+# end
+# 
+# 99.times do
+	# create_landlords(db, Faker::Name.name, hood_selector, rand(500..5000), rand(0..5), rand(0..5), ["true", "false"].sample)
+# end
 
 
 
