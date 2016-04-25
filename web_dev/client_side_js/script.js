327f8b131c5b1f48bf6ba9836e841dd6a55fa82d
SangMeeSpecht <spechtse@miamioh.edu>
2016-04-25 13:45:38 -0500

    Add button

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -8,3 +8,10 @@ add_items.appendChild(node);
 
 var add_exercise = document.getElementById("home_exercises");
 add_exercise.appendChild(add_items);
+
+var btn = document.createElement("BUTTON");
+var node2 = document.createTextNode("Add Exercise");
+btn.appendChild(node2);
+
+var add_more_exercise = document.getElementById("home_exercises");
+add_more_exercise.appendChild(btn);
