51a61b2fe2cb67653dbf281c341a53bf2a97cc99
SangMeeSpecht <spechtse@miamioh.edu>
2016-04-25 15:05:25 -0500

    Release 2: Add exercise when clicked

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -1,28 +1,26 @@
 console.log("Connecting files");
 
-
+// Release 1
 var add_items = document.createElement("li");
 var node = document.createTextNode("Lunges");
 add_items.appendChild(node);
 
-
 var add_exercise = document.getElementById("home_exercises");
 add_exercise.appendChild(add_items);
 
-var btn = document.createElement("BUTTON");
-var node2 = document.createTextNode("Add Exercise");
-btn.appendChild(node2);
-
-var add_more_exercise = document.getElementById("home_exercises");
-add_more_exercise.appendChild(btn);
-
+// Release 2
 function addExercises(event) {
 	// console.log("Click on the button to add more exercises.");
 	console.log(event);
-	event.target.style.border = "4px solid green";
+	var add_items = document.createElement("li");
+	var node = document.createTextNode("Dead lifts");
+	add_items.appendChild(node);
+
+	var add_exercise = document.getElementById("home_exercises");
+	add_exercise.appendChild(add_items);
 }
 
-var test = document.getElementById("home_exercises");
+var test = document.getElementById("add_exercises");
 test.addEventListener("click", addExercises);
 
 
