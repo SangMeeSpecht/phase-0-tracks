600a1acb0214f6e204ca85bc48f3834887936dae
SangMeeSpecht <spechtse@miamioh.edu>
2016-04-25 15:13:16 -0500

    Add release description

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -1,6 +1,7 @@
 console.log("Connecting files");
 
 // Release 1
+// Add another list element
 var add_items = document.createElement("li");
 var node = document.createTextNode("Lunges");
 add_items.appendChild(node);
@@ -9,6 +10,7 @@ var add_exercise = document.getElementById("home_exercises");
 add_exercise.appendChild(add_items);
 
 // Release 2
+// Add another list element with an event listener
 function addExercises(event) {
 	var exerciseOptions = ["Dead lifts", "Eliptical", "Crunches"];
 	var rand = exerciseOptions[Math.floor(Math.random() * exerciseOptions.length)];
