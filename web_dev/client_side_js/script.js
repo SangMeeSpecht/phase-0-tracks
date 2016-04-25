42406d2f32d31ece14b1dad9132edfb35ec805fa
SangMeeSpecht <spechtse@miamioh.edu>
2016-04-25 13:35:23 -0500

    Add Release 1

---
+++ web_dev/client_side_js/script.js
@@ -0,0 +1,10 @@
+console.log("Connecting files")
+
+
+var add_items = document.createElement("li");
+var node = document.createTextNode("Lunges");
+add_items.appendChild(node);
+
+
+var add_exercise = document.getElementById("home_exercises");
+add_exercise.appendChild(add_items);
